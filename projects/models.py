8db3897b3959277b6109e02840978f16f8a81d3e
Slimarius <212497123+SlimariusSlimus@users.noreply.github.com>
2026-02-19 11:47:26 +0100

    feat: FinishedProject model; tests: some tests for FinishedProject; saving to merge from main

--- projects/models.py
+++ projects/models.py
@@ -33,7 +33,7 @@ class ProjectIdea(models.Model):
 
 class ProjectGroup(models.Model):
     """
-    This is a project group whuch was created under a project idea. It can be given a name and description
+    This is a project group which was created under a project idea. It can be given a name and description
     """
 
     name = models.CharField(max_length=200, null=False)
@@ -51,3 +51,19 @@ class ProjectGroup(models.Model):
 
     def __str__(self):
         return f"Project Group: '{self.name}' Created under: '{self.project_idea.title}' Created on: {self.created_on}"
+
+
+class FinishedProject(models.Model):
+    """This is the finished project of a group, stemming from a ProjectIdea"""
+
+    title = models.CharField(max_length=200)
+    # project groups are linked to the idea, so we don't need an additional reference
+    project_group = models.ForeignKey(ProjectGroup, on_delete=models.SET_NULL, null=True, related_name='groups_finished_projects')
+    description = models.TextField()
+    finished_on = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='tags_finished_projects')
+    likes = models.ManyToManyField(User, related_name='likes_finished_projects')
+
+    def __str__(self):
+        group = self.project_group if self.project_group else "Deleted Group"
+        return f"Project: '{self.title}' Submitted by: '{group.name}' Finished on on: {self.finished_on}"
---
+++ projects/tests/test_models/test_finished_project_model.py
@@ -0,0 +1,152 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
+from projects.models import ProjectIdea, ProjectGroup, FinishedProject, Tag
+from django.utils import timezone
+from datetime import timedelta
+
+User = get_user_model()
+
+class FinishedProjectModelTests(TestCase):
+    def setUp(self):
+        # create test user
+        self.user = User.objects.create_user(username="owner", password="password")
+        # create test project_idea
+        self.project_idea = ProjectIdea.objects.create(
+            title="Test idea",
+            author=self.user,
+            description="Descriptive text",
+        )
+        # create test group
+        self.project_group = ProjectGroup.objects.create(
+            name="Test group",
+            project_idea=self.project_idea,
+            owner=self.user
+        )
+        # create test finished_project
+        self.finished_project = FinishedProject.objects.create(
+            title="Test finished project",
+            description="Descriptive text",
+            project_group=self.project_group
+        )
+
+    ### VALID
+    def test_create_finished_project(self):
+        """Verify, that the FinishedProject gets created correctly and without error"""
+        self.assertEqual(self.finished_project.title, "Test finished project")
+        self.assertEqual(self.finished_project.description, "Descriptive text")
+        self.assertEqual(self.finished_project.project_group, self.project_group)
+
+    def test_finished_on_correct_timestamp(self):
+        """Verify that timestamps are generated correctly"""
+        self.assertIsNotNone(self.finished_project.finished_on)
+        now = timezone.now()
+        # we use AlmostEqual, because we can't compare accurately on a detailed level because the code takes time to run
+        self.assertAlmostEqual(
+            self.finished_project.finished_on,
+            now,
+            delta=timedelta(minutes=1)
+        )
+
+    def test_finished_project_str_method(self):
+        """Verify the correctness of the string returned by the __str__ method"""
+        # we don't test for the timestamp here. If the rest works, so should the timestamp and we would
+        # have similar problems comparing as in the timestamp test itself
+        self.assertIn(
+            f"Project: 'Test finished project' Submitted by: '{self.project_group.name}'",
+            str(self.finished_project)
+        )
+
+#     def test_project_idea_str_method_deleted_user(self):
+#         """Verify that the __str__ method replaces the username of a deleted user correctly with 'Deleted User'"""
+#         # we don't test for the timestamp here. If the rest works, so should the timestamp and we would
+#         # have similar problems comparing as in the timestamp test itself
+#         self.user.delete()
+
+#         # sync the object in memory with the database state (python object isn't updated after we delete from db)
+#         self.finished_project.refresh_from_db()
+
+#         self.assertIn(
+#             "Project: 'Test Idea' Submitted by: 'Deleted User'",
+#             str(self.finished_project))
+
+#     def test_post_updates_correctly_after_user_deletion(self):
+#         """Verify that the author is deleted after user deletion but the post remains in db"""
+#         self.user.delete()
+#         # refresh the idea instance from the database
+#         self.finished_project.refresh_from_db()
+
+#         # author should now be "Deleted User"
+#         self.assertIsNone(self.finished_project.author)
+#         self.assertTrue(ProjectIdea.objects.filter(id=self.finished_project.id).exists())
+
+#     ### INVALID
+#     def test_create_project_idea_title_too_long(self):
+#         """Ensure that a title exceeding max_length raises a ValidationError"""
+#         # we're using the ProjectIdea constructor instead of .objects.create, so we don't attempt to write
+#         # directly into the database, which can lead to problems in eg. SQLite
+#         project_idea = ProjectIdea(
+#             title="A" * 201,
+#             author=self.user,
+#             description="Descriptive text",
+#         )
+#         with self.assertRaises(ValidationError):
+#             # this triggers model constraint checks
+#             project_idea.full_clean()
+
+
+# class ProjectIdeaModelTagTests(TestCase):
+#     def setUp(self):
+#         self.tag_python = Tag.objects.create(name="python")
+#         self.tag_automation = Tag.objects.create(name="automation")
+#         self.project = ProjectIdea.objects.create(
+#             title="A python powered automation tool",
+#             description="Automates mundane tasks with a python script"
+#         )
+
+#     ### VALID
+#     def test_add_tags_to_project(self):
+#         """Ensures that multiple tags can be added to a project"""
+#         self.project.tags.add(self.tag_python, self.tag_automation)
+
+#         self.assertEqual(self.project.tags.count(), 2)
+#         self.assertIn(self.tag_python, self.project.tags.all())
+#         self.assertIn(self.tag_automation, self.project.tags.all())
+
+#     def test_remove_tag_from_project(self):
+#         """Ensures that a tag can be removed from a project and others persist"""
+#         self.project.tags.add(self.tag_python, self.tag_automation)
+
+#         self.project.tags.remove(self.tag_automation)
+
+#         self.assertEqual(self.project.tags.count(), 1)
+#         self.assertIn(self.tag_python, self.project.tags.all())
+#         self.assertNotIn(self.tag_automation, self.project.tags.all())
+
+#         # ensure the Tag object still exists in the db
+#         self.assertTrue(Tag.objects.filter(name="automation").exists())
+
+
+# class ProjectIdeaModelLikesTests(TestCase):
+#     def setUp(self):
+#         self.user = User.objects.create_user(username="user", password="password")
+#         self.project = ProjectIdea.objects.create(
+#             title="Universally liked project",
+#             description="Everybody loves this!"
+#         )
+
+#     def test_user_likes_project(self):
+#         """Ensure that adding a User to the likes works and increases the counter"""
+#         self.project.likes.add(self.user)
+
+#         self.assertEqual(self.project.likes.count(), 1)
+#         self.assertIn(self.user, self.project.likes.all())
+
+#     def test_user_likes_project_twice(self):
+#         """
+#         Checks that liking a post twice doesn't do anything
+#         (Frontend should handle that but desync can do all sorts of stuff)
+#         """
+#         self.project.likes.add(self.user)
+#         self.project.likes.add(self.user)
+#         self.assertEqual(self.project.likes.count(), 1)
