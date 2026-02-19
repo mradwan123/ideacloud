85921f418d7bf99a5b560e6c1ea6467406ab9d67
Slimarius <212497123+SlimariusSlimus@users.noreply.github.com>
2026-02-19 13:53:07 +0100

    fix: small fix in project_idea tests; tests: finished tests for finished_project

--- projects/tests/test_models/test_finished_project_model.py
+++ projects/tests/test_models/test_finished_project_model.py
@@ -58,93 +58,126 @@ class FinishedProjectModelTests(TestCase):
         )
 
     def test_finished_project_str_method_deleted_group(self):
-        """Verify that the __str__ method replaces the username of a deleted user correctly with 'Deleted Group'"""
-        self.user.delete()
+        """Verify that the __str__ method replaces the group name of a deleted group correctly with 'Deleted Group'"""
+        self.project_group.delete()
 
-        # sync the object in memory with the database state (python object isn't updated after we delete from db)
+        # refresh the project instance from the database
         self.finished_project.refresh_from_db()
 
         self.assertIn(
             "Project: 'Test finished project' Submitted by: 'Deleted Group'",
             str(self.finished_project))
 
-    def test_finished_project_updates_correctly_after_user_deletion(self):
-        """Verify that the author is deleted after user deletion but the post remains in db"""
-        self.user.delete()
-        # refresh the idea instance from the database
+    def test_finished_project_updates_correctly_after_group_deletion(self):
+        """Verify that the group is deleted after group deletion but the FinishedProject remains in db"""
+        self.project_group.delete()
+        # refresh the Project instance from the database
         self.finished_project.refresh_from_db()
 
-        # author should now be "Deleted User"
-        self.assertIsNone(self.finished_project.author)
-        self.assertTrue(ProjectIdea.objects.filter(id=self.finished_project.id).exists())
+        # group should now be NULL
+        self.assertIsNone(self.finished_project.project_group)
+        # project should still exist
+        self.assertTrue(FinishedProject.objects.filter(id=self.finished_project.id).exists())
 
-#     ### INVALID
-#     def test_create_project_idea_title_too_long(self):
-#         """Ensure that a title exceeding max_length raises a ValidationError"""
-#         # we're using the ProjectIdea constructor instead of .objects.create, so we don't attempt to write
-#         # directly into the database, which can lead to problems in eg. SQLite
-#         project_idea = ProjectIdea(
-#             title="A" * 201,
-#             author=self.user,
-#             description="Descriptive text",
-#         )
-#         with self.assertRaises(ValidationError):
-#             # this triggers model constraint checks
-#             project_idea.full_clean()
-
-
-# class ProjectIdeaModelTagTests(TestCase):
-#     def setUp(self):
-#         self.tag_python = Tag.objects.create(name="python")
-#         self.tag_automation = Tag.objects.create(name="automation")
-#         self.project = ProjectIdea.objects.create(
-#             title="A python powered automation tool",
-#             description="Automates mundane tasks with a python script"
-#         )
-
-#     ### VALID
-#     def test_add_tags_to_project(self):
-#         """Ensures that multiple tags can be added to a project"""
-#         self.project.tags.add(self.tag_python, self.tag_automation)
-
-#         self.assertEqual(self.project.tags.count(), 2)
-#         self.assertIn(self.tag_python, self.project.tags.all())
-#         self.assertIn(self.tag_automation, self.project.tags.all())
-
-#     def test_remove_tag_from_project(self):
-#         """Ensures that a tag can be removed from a project and others persist"""
-#         self.project.tags.add(self.tag_python, self.tag_automation)
-
-#         self.project.tags.remove(self.tag_automation)
-
-#         self.assertEqual(self.project.tags.count(), 1)
-#         self.assertIn(self.tag_python, self.project.tags.all())
-#         self.assertNotIn(self.tag_automation, self.project.tags.all())
-
-#         # ensure the Tag object still exists in the db
-#         self.assertTrue(Tag.objects.filter(name="automation").exists())
+    ### INVALID
+    def test_create_finished_project_title_too_long(self):
+        """Ensure that a title exceeding max_length raises a ValidationError"""
+        # we're using the FinishedProject constructor instead of .objects.create, so we don't attempt to write
+        # directly into the database, which can lead to problems in eg. SQLite
+        finished_project = FinishedProject(
+            title="A" * 201,
+            project_group=self.project_group,
+            description="Descriptive text",
+        )
+        with self.assertRaises(ValidationError):
+            # this triggers model constraint checks
+            finished_project.full_clean()
 
 
-# class ProjectIdeaModelLikesTests(TestCase):
-#     def setUp(self):
-#         self.user = User.objects.create_user(username="user", password="password")
-#         self.project = ProjectIdea.objects.create(
-#             title="Universally liked project",
-#             description="Everybody loves this!"
-#         )
+class FinishedProjectModelTagTests(TestCase):
+    def setUp(self):
+        # create test user
+        self.user = User.objects.create_user(username="owner", password="password")
+        # create test project_idea
+        self.project_idea = ProjectIdea.objects.create(
+            title="Test idea",
+            author=self.user,
+            description="Descriptive text",
+        )
+        # create test group
+        self.project_group = ProjectGroup.objects.create(
+            name="Test group",
+            project_idea=self.project_idea,
+            owner=self.user
+        )
+        # create test tags
+        self.tag_python = Tag.objects.create(name="python")
+        self.tag_automation = Tag.objects.create(name="automation")
+        # create test finished project
+        self.finished_project = FinishedProject.objects.create(
+            title="A python powered automation tool",
+            project_group=self.project_group,
+            description="Automates mundane tasks with a python script",
+        )
 
-#     def test_user_likes_project(self):
-#         """Ensure that adding a User to the likes works and increases the counter"""
-#         self.project.likes.add(self.user)
+    ### VALID
+    def test_add_tags_to_finished_project(self):
+        """Ensures that multiple tags can be added to a finished project"""
+        self.finished_project.tags.add(self.tag_python, self.tag_automation)
 
-#         self.assertEqual(self.project.likes.count(), 1)
-#         self.assertIn(self.user, self.project.likes.all())
+        self.assertEqual(self.finished_project.tags.count(), 2)
+        self.assertIn(self.tag_python, self.finished_project.tags.all())
+        self.assertIn(self.tag_automation, self.finished_project.tags.all())
 
-#     def test_user_likes_project_twice(self):
-#         """
-#         Checks that liking a post twice doesn't do anything
-#         (Frontend should handle that but desync can do all sorts of stuff)
-#         """
-#         self.project.likes.add(self.user)
-#         self.project.likes.add(self.user)
-#         self.assertEqual(self.project.likes.count(), 1)
+    def test_remove_tag_from_finished_project(self):
+        """Ensures that a tag can be removed from a finished project and others persist"""
+        self.finished_project.tags.add(self.tag_python, self.tag_automation)
+
+        self.finished_project.tags.remove(self.tag_automation)
+
+        self.assertEqual(self.finished_project.tags.count(), 1)
+        self.assertIn(self.tag_python, self.finished_project.tags.all())
+        self.assertNotIn(self.tag_automation, self.finished_project.tags.all())
+
+        # ensure the Tag object still exists in the db
+        self.assertTrue(Tag.objects.filter(name="automation").exists())
+
+
+class FinishedProjectModelLikesTests(TestCase):
+    def setUp(self):
+        # create test user
+        self.user = User.objects.create_user(username="owner", password="password")
+        # create test project_idea
+        self.project_idea = ProjectIdea.objects.create(
+            title="Test idea",
+            author=self.user,
+            description="Descriptive text",
+        )
+        # create test group
+        self.project_group = ProjectGroup.objects.create(
+            name="Test group",
+            project_idea=self.project_idea,
+            owner=self.user
+        )
+        # create test finished project
+        self.finished_project = FinishedProject.objects.create(
+            title="A python powered automation tool",
+            project_group=self.project_group,
+            description="Automates mundane tasks with a python script",
+        )
+
+    def test_user_likes_project(self):
+        """Ensure that adding a User to the likes works and increases the counter"""
+        self.finished_project.likes.add(self.user)
+
+        self.assertEqual(self.finished_project.likes.count(), 1)
+        self.assertIn(self.user, self.finished_project.likes.all())
+
+    def test_user_likes_project_twice(self):
+        """
+        Checks that liking a post twice doesn't do anything
+        (Frontend should handle that but desync can do all sorts of stuff)
+        """
+        self.finished_project.likes.add(self.user)
+        self.finished_project.likes.add(self.user)
+        self.assertEqual(self.finished_project.likes.count(), 1)
