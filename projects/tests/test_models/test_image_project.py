3bd00f19557d3b6ff4d78dfec248e4bf8f1c852d
Radwan <radwan@duck.com>
2026-02-19 12:51:50 +0200

    test:added upload test and foreign ey test for image model in projects app

--- projects/tests/test_models/test_image_project.py
+++ projects/tests/test_models/test_image_project.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
-from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from ...models import ImageProject, ProjectIdea
-from django.db.utils import IntegrityError
-    
+import os    
 
 class ImageProjectTest(TestCase):
     def setUp(self):
@@ -20,6 +18,10 @@ class ImageProjectTest(TestCase):
            
         )
     def test_image_upload(self):
+        """
+        Project images are uploaded to /project_images/. This test uses dummy files and test 
+        start of the path with project_images/test_image
+        """
         #create dummy file
         dummy_file = SimpleUploadedFile(
             name="test_image.jpg",
@@ -29,7 +31,14 @@ class ImageProjectTest(TestCase):
         
         image_project = ImageProject.objects.create(project_idea=self.project_idea,
                                                     image=dummy_file)                                     
-        self.assertTrue(image_project.image.name.endswith('test_image.jpg'))
-    
- 
+        self.assertTrue(image_project.image.name.startswith('project_images/test_image'),)
+        self.assertTrue(os.path.exists(image_project.image.path))
+        
+        #Checking foreign key
+        self.assertIsNotNone(image_project.pk)
+
+
+
+   
+
  
