cf338271f7dd04e0e5f33e9296613bbcdf1e86d3
Slimarius <212497123+SlimariusSlimus@users.noreply.github.com>
2026-02-20 13:22:37 +0100

    feat: added A ProfanityValidator; tests: wrote tests for said validator

--- projects/serializers/serializer_profanity_validator.py
+++ projects/serializers/serializer_profanity_validator.py
@@ -1,20 +1,20 @@
 import json
 from pathlib import Path
 import re
+from django.conf import settings
 from rest_framework.exceptions import ValidationError
 
 def load_profane_words():
     """Imports a list of banned words from a json file"""
-    current_file = Path(__file__).resolve()
-    file_path = current_file.parent.parent / 'resources' / 'profanity_words_list.json'
+    file_path = Path(settings.BASE_DIR) / 'projects' / 'resources' / 'profanity_words_list.json'
 
     try:
         with open(file_path, 'r') as f:
             banned_words = json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
+    except (FileNotFoundError, json.JSONDecodeError) as e:
         # fallback to an empty list if file is missing or broken
         banned_words = []
-
+        print(f"\n[ProfanityValidator] Error loading file at {file_path}: {e}")  # DEBUG statement. Keep out of prod!
     return banned_words
 
 
@@ -29,7 +29,7 @@ class ProfanityValidator:
 
     def __call__(self, value):
         # DEBUG: See what is actually being loaded
-        print(f"Checking against: {self.profane_words}")
+        # print(f"Checking against: {self.profane_words}")
         for word in self.profane_words:
             # the /b ensures that we only check for full words, not contained inside a word (like 'Tit' in 'Title')
             # re.escape() ensures that special characters are escaped
