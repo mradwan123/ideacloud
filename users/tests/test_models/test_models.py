122c61f58e11478373582b66d632dfcb8049aa88
Lamarr12345 <richter_malte@web.de>
2026-02-20 12:01:53 +0100

    feat: added test for custom profile image

--- users/tests/test_models/test_models.py
+++ users/tests/test_models/test_models.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from ...models import User
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
@@ -6,11 +6,35 @@ from django.db import IntegrityError
 from django.utils import timezone
 from datetime import timedelta
 from django.conf import settings
+import os    
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
+import shutil
+import tempfile
 
 
+# Create a temporary media directory for tests
+TEMP_MEDIA_ROOT = tempfile.mkdtemp()
 
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class UserModelTest(TestCase):
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        if os.path.exists(TEMP_MEDIA_ROOT):
+            shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+            
+    def _create_test_image(self):
+        """Helper to create a temporary image file"""
+        image = Image.new('RGB', (100, 100), color='red')
+        buffer = BytesIO()
+        image.save(buffer, format='JPEG')
+        buffer.seek(0)
+        return SimpleUploadedFile('profile_test.jpg', buffer.read(), content_type='image/jpeg')
     
+    #TODO change create to create user and make tests pass
     def setUp(self):
         self.user = User.objects.create(
             username = 'testusername',
@@ -54,6 +78,19 @@ class UserModelTest(TestCase):
         default_path = str(settings.MEDIA_ROOT) + "/profile_images/default.jpg"
         user_path = self.user.image.path
         self.assertEqual(default_path, user_path)
+
+    def test_user_custom_image_path(self):
+        new_profile_image = self._create_test_image()
+        new_user = User.objects.create_user(username='test_image_user',
+                                   email='test_image_user@ideacloud.com',
+                                   password='idea123',
+                                   image=new_profile_image,
+                                   description='some texts',
+                                   created_on='2026-02-19',
+                                   available=False)
+        default_path = str(settings.MEDIA_ROOT) + "/profile_images/profile_test"
+        user_path = new_user.image.path
+        self.assertIn(default_path, user_path)
             
     def test_username_unique_constraint(self): #Abstractuser class includes by default unique username
         User.objects.create_user(username='george', email='a@locospace.com', password='password')
