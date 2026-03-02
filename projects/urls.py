52185878a4e7153305a7618b59837a25ce6e7395
Rad1 <muhammadradwan@Air-Malware.local>
2026-03-02 17:00:26 +0200

    feat: added url paths for finsihed project and finsihed project detail

--- projects/urls.py
+++ projects/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from .views.test_view_to_display_images import test_image_view
 from projects.views.view_project_idea import ProjectIdeaList, ProjectIdeaDetail
 from projects.views.view_project_group import ProjectGroupList, ProjectGroupDetail
+from projects.views.view_finished_project import FinishedProject, FinishedProjectDetail
 
 app_name = "projects"
 urlpatterns = [
@@ -16,6 +17,13 @@ urlpatterns = [
     path("project-ideas/<int:idea_pk>/project-groups/", ProjectGroupList.as_view(), name="project-group-list"),
     # Access to methods related to a specific group
     path("project-ideas/<int:idea_pk>/project-groups/<int:group_pk>/", ProjectGroupDetail.as_view(), name="project-group-detail"),
+
+    ## FinishedProject
+    # Listing all finished projects or create a new one
+    path("finished-projects/", FinishedProject.as_view(), name="finished-project-list"),
+    # Access to get/path/del methods related to a specific finished project
+    path("finished-projects/<int:finished_pk>/", FinishedProjectDetail.as_view(), name="finished-project-detail"),
+    
 
     ## Images
     # testing view for images
