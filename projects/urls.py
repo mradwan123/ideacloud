a6e2d6a4ff4e2c5149a403a8cab1af3fca9ed3b1
Slimarius <212497123+SlimariusSlimus@users.noreply.github.com>
2026-03-03 14:05:35 +0100

    feat: created views for like toggling

--- projects/urls.py
+++ projects/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from .views.test_view_to_display_images import test_image_view
 from projects.views.view_project_idea import ProjectIdeaList, ProjectIdeaDetail
 from projects.views.view_project_group import ProjectGroupList, ProjectGroupDetail
+from projects.views.view_likes import ProjectIdeaToggleLike, FinishedProjectToggleLike
 
 app_name = "projects"
 urlpatterns = [
@@ -12,10 +13,18 @@ urlpatterns = [
     path("project-ideas/", ProjectIdeaList.as_view(), name="project-idea-list"),
     # Access to methods related to a specific idea
     path("project-ideas/<int:idea_pk>/", ProjectIdeaDetail.as_view(), name="project-idea-detail"),
+
+    ## ProjectGroup
     # Listing all groups under an idea or create a new one
     path("project-ideas/<int:idea_pk>/project-groups/", ProjectGroupList.as_view(), name="project-group-list"),
     # Access to methods related to a specific group
     path("project-ideas/<int:idea_pk>/project-groups/<int:group_pk>/", ProjectGroupDetail.as_view(), name="project-group-detail"),
+
+    ##Likes
+    # Toggling a user's like on Project Ideas
+    path("project-ideas/<int:idea_pk>/like/", ProjectIdeaToggleLike.as_view(), name="project-idea-like"),
+    # Toggling a user's like on Finished Projects
+    path("finished-projects/<int:project_pk>/like/", FinishedProjectToggleLike.as_view(), name="finished-project-like"),
 
     ## Images
     # testing view for images
