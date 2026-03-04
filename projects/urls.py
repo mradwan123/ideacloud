b36a768d4bfef4072d187a7141d62fc882ad820f
Slimarius <212497123+SlimariusSlimus@users.noreply.github.com>
2026-03-04 14:26:36 +0100

    tests: finished setting up tests for likes

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views.test_view_to_display_images import test_image_view
 from projects.views.view_project_idea import ProjectIdeaList, ProjectIdeaDetail
 from projects.views.view_project_group import ProjectGroupList, ProjectGroupDetail
 from projects.views.view_likes import ProjectIdeaToggleLike, FinishedProjectToggleLike
@@ -25,8 +24,4 @@ urlpatterns = [
     path("project-ideas/<int:idea_pk>/like/", ProjectIdeaToggleLike.as_view(), name="project-idea-like"),
     # Toggling a user's like on Finished Projects
     path("finished-projects/<int:project_pk>/like/", FinishedProjectToggleLike.as_view(), name="finished-project-like"),
-
-    ## Images
-    # testing view for images
-    path("test_image/", view=test_image_view, name="image_test"),
 ]
