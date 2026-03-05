abf1a411831292f091dafbb7178aa2e778623e64
Radwan <radwan@duck.com>
2026-03-05 11:06:56 +0200

    config settings for infinte forest'

--- config/settings.py
+++ config/settings.py
@@ -35,9 +35,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6niu^40cw#4#@ozotbp&e8t!nxwoi-
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'aqueous-crag-51920-280fe96ba7c2.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'infinite-forest-53358.herokuapp.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://aqueous-crag-51920-280fe96ba7c2.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['https://infinite-forest-53358.herokuapp.com/']
 
 # Application definition
 
