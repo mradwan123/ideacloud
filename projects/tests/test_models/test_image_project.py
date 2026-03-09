16c20a8e2383b6c46b8927f67ab36e6b5c1b1555
Lamarr12345 <richter_malte@web.de>
2026-03-09 14:04:09 +0100

    fix: project idea image string representation

--- projects/tests/test_models/test_image_project.py
+++ projects/tests/test_models/test_image_project.py
@@ -29,7 +29,7 @@ class ImageProjectTest(TestCase):
         buffer = BytesIO()
         image.save(buffer, format='JPEG')
         buffer.seek(0)
-        return SimpleUploadedFile('view_test.jpg', buffer.read(), content_type='image/jpeg')
+        return SimpleUploadedFile('test_image.jpg', buffer.read(), content_type='image/jpeg')
 
     def setUp(self):
         User = get_user_model()
@@ -43,28 +43,25 @@ class ImageProjectTest(TestCase):
             description="Descriptive text",
            
         )
+
     def test_image_upload(self):
         """
         Project images are uploaded to /project_images/. This test uses dummy files and test 
         start of the path with project_images/test_image
         """
-        #create dummy file
-        dummy_file = SimpleUploadedFile(
-            name="test_image.jpg",
-            content=b"fake image bytes",
-            content_type="image/jpeg",)
-        
-        
+
         image_project = ImageProject.objects.create(project_idea=self.project_idea,
-                                                    image=dummy_file)                                     
-        self.assertTrue(image_project.image.name.startswith('project_images/test_image'),)
+                                                    image=self._create_test_image())
+        self.assertTrue(image_project.image.name.startswith("project_images/test_image"))
         self.assertTrue(os.path.exists(image_project.image.path))
-        
+
         #Checking foreign key
         self.assertIsNotNone(image_project.pk)
 
-
-
-   
-
- 
+    def test_image_string_representation(self):
+        """Checks if the image field is correctly represented as a using the string method."""
+        image_project = ImageProject.objects.create(project_idea=self.project_idea,
+                                                    image=self._create_test_image())
+        string_rep = str(image_project)
+        self.assertIn("Name: ", string_rep)
+        self.assertIn("Project Idea: ", string_rep)
